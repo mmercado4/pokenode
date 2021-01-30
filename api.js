e88f86b4ee672da21f4f65d7817bb54053d08d1f
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-01-30 12:42:06 +0100

    POST method created

--- api.js
+++ api.js
@@ -1,19 +1,71 @@
 const express = require("express");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const api = express();
 
+api.use(bodyParser.urlencoded({ extended: true })); //Decodificamos la info del body.
+
+//GET
 api.get("/api/pokemons", (request, response) => {
   fs.readFile("db/dbPokemon.json", (error, data) => {
     if (error) throw error; //Similar al console.error. Elevar o notificar una excepción.
     const pokemonJSON = JSON.parse(data); //Data no lo lee como un JSON, sino como texto plano.
     response.status(200).send({
       success: true,
-      message: "/api/pokemons",
+      url: "/api/pokemons",
       method: "GET",
       pokemons: pokemonJSON,
     });
   });
+});
+
+//POST por parámetros "api/pokemons?name=pikachu&type=electrico"
+api.post("/api/pokemons", (request, response) => {
+  if (!request.body.name || !request.body.type) {
+    response.status(400).send({
+      success: false,
+      url: "/api/pokemons",
+      method: "POST",
+      message: "Name and Type are required",
+    });
+  } else {
+    // En request.query tenemos todos los parámetros que se envíen al microservicio.
+    fs.readFile("db/dbPokemon.json", (error, data) => {
+      if (error) throw error;
+      const allPokemons = JSON.parse(data);
+      const newPokemon = {
+        id: allPokemons.length + 1,
+        name: request.body.name,
+        type: request.body.type,
+      };
+      allPokemons.push(newPokemon);
+
+      fs.writeFile(
+        "db/dbPokemon.json",
+        JSON.stringify(allPokemons),
+        (error) => {
+          if (error) {
+            response.status(400).send({
+              success: false,
+              url: "/api/pokemons",
+              method: "POST",
+              message: "Fallo al añadir el pokemon",
+              error: error,
+            });
+          } else {
+            response.status(201).send({
+              success: true,
+              url: "/api/pokemons",
+              method: "POST",
+              message: "Pokemon añadido correctamente",
+              newPokemon: newPokemon,
+            });
+          }
+        }
+      );
+    });
+  }
 });
 
 api.listen(1212, () => {
